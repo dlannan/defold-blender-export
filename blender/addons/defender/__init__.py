64bbff6ebe7815f8acfd805a67970b14828fba96
WIN-FT9HE12U6H0\dlannan <david.lannan@calytrix.com>
2023-11-12 15:10:56 +1030

    Updated version. Was incorrect

--- blender/addons/defender/__init__.py
+++ blender/addons/defender/__init__.py
@@ -43,7 +43,7 @@ bl_info = {
     "name": "Defender",
     "description": "Sync a Blender Scene directly to Defold resources",
     "author": "dlannan",
-    "version": (1, 3, 2),
+    "version": (1, 3, 4),
     "blender": (2, 80, 0),
     "location": "3D View > Defold",
     "warning": "", # used for warning icon and text in addons panel
